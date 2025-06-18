f9c4e1f78e75fac9c89ddf2378a28f74cb36b451
Espen Wiborg <espenhw@grumblesmurf.org>
2025-06-19 01:26:39 +0200

    2

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,8 @@
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+import CleanCSS from "clean-css";
+
 export default async function(eleventyConfig) {
   eleventyConfig.setInputDirectory("input")
-  eleventyConfig.addPassthroughCopy("styles.css");
 	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
 		return dateObj.toISOString().substring(0, 10);
 	});
@@ -9,4 +10,7 @@ export default async function(eleventyConfig) {
     coll.getFilteredByTag("dikt").sort((a, b) => a.inputPath.localeCompare(b.inputPath))
   )
   eleventyConfig.amendLibrary("md", (mdLib) => mdLib.enable("code"));
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 };
